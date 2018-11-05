da55d3366e43b294cd5196282e408c0b9111ee6d
Hyejin Ahn <hyejinahn0619@gmail.com>
2018-11-05 23:36:19 +0900

    add sample end point for user model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,18 @@
 module Api
   module V1
     class UsersController < ApplicationController
+    	include RateLimit
       def index
         users = User.all
-        render json: users, status: :ok
+      	if res = reach_max_request?(request.remote_ip)
+        	render json: { message: "You have fired too many requests. Please wait for #{res} seconds", status: 429	}
+        else
+        	render json: users, status: :ok
+        end
       end
     end
   end
 end
+
+
+
