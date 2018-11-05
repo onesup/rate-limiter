b4f6a5d896415d29a15d20b929a9c7c2449e4600
Hyejin Ahn <hyejinahn0619@gmail.com>
2018-11-05 23:35:51 +0900

    add rate limit module

---
+++ lib/rate_limit.rb
@@ -0,0 +1,29 @@
+module RateLimit
+	COUNTING_EXPIRY_TIME = 30
+	COUNT_LIMIT = 5
+	BLOCKED_EXPIRY_TIME = 30
+
+	def reach_max_request?(key)
+		# check if this key is a blocked key
+		if $redis.get("blocked_#{key}")
+			seconds_left = $redis.ttl("blocked_#{key}")
+			return seconds_left
+		end		
+
+		# get and increase the value for the key
+		# if the key doesn't exist, the value will be set as 1
+		request_count = $redis.incr("counting_#{key}")
+
+		# check if the request count is greater than the limit
+		if request_count > COUNT_LIMIT
+			$redis.setex("blocked_#{key}", BLOCKED_EXPIRY_TIME, true)
+			$redis.del("counting_#{key}")
+
+			seconds_left = $redis.ttl("blocked_#{key}")
+			return seconds_left
+		else
+			$redis.expire("counting_#{key}", COUNTING_EXPIRY_TIME)
+		end
+		return false
+	end
+end
